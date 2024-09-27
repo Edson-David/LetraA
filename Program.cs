3ee90fa5d654ffb8f4a0ae9c8815527eb0ac0c15
Edson-David <edsondavid22@outlook.com>
2024-09-27 18:54:12 -0300

    Verifica letra A

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LetraA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma string: ");
+            string texto = Console.ReadLine();
+           
+            texto = texto.ToLower();
+
+            int letraA = 0;
+                        
+            foreach (char caractere in texto)
+            {
+                if (caractere == 'a')
+                {
+                    letraA++;
+                }
+            }
+
+            if (letraA > 0)
+            {
+                Console.WriteLine( "A letra 'a' aparece " + letraA + " vezes na string." );
+            }
+            else
+            {
+                Console.WriteLine( "A letra 'a' não foi encontrada na string." );
+            }
+        }
+    }
+}
